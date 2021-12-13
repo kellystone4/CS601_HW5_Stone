48ab8f3ee44f10220f357ed4ea5352adfd290485
Kelly Stone <kellystone916@gmail.com>
2021-12-13 04:18:40 -0800

    commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
           .then((data) =>
             document.write(
                 // `Hello, welcome to my site! Learn about my degree's here: `
-              `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.school} in ${data.data[0].degrees.bachelors.data.program}`
+            //   `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.school} in ${data.data[0].degrees.bachelors.data.program}`
               `My second degree was a ${data.data[0].degrees.masters.data.type} from ${data.data[0].degrees.masters.data.school} in ${data.data[0].degrees.masters.data.program}`
 
             )
