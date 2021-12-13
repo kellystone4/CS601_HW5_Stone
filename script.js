f8f8e504bb557d535fd32cbf903a11aefe485c8e
Kelly Stone <kellystone916@gmail.com>
2021-12-13 03:54:39 -0800

    adding netlify http

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-// var clickBtn = document.getElementById("clickHere")
+const clickBtn = document.querySelector("#clickHere")
+const result = document.querySelector('#result')
+
+// gets data from API and sets the content of #result div
+const getData = async function() {
+    await fetch("https://kellystone.netlify.app/")
+          //get your data here, and check for the response status. If it's not 200, throw an error
+          .then((response) => response.json())
+          .then(data => {
+            result.innerText = JSON.stringify(data.data[0].degrees.masters.data.type)
+          })
+        //   .then((data) =>
+        //     document.write(
+        //       `My first degree was a ${data.data[0].degrees.masters.data.type} from ${data.data[0].degrees.masters.data.school} in ${data.data[0].degrees.masters.data.program}`
+        //     )
+        //   )
+    .catch(error => console.log(error))
+}
 
 // if (onclick.clickBtn) {
-async function getDegrees(url) {
-    // fetch the url
-    await fetch(url)
-      //get your data here, and check for the response status. If it's not 200, throw an error
-      .then((response) => response.json())
-      .then((data) =>
-        document.write(
-          `My first degree was a ${data.data[0].degrees.masters.data.type} from ${data.data[0].degrees.masters.data.school} in ${data.data[0].degrees.masters.data.program}`
-        )
-      )
-        getDegrees("./degrees.json");
-  }
+// async function getDegrees(url) {
+//     // fetch the url
+//     await fetch(url)
+//       //get your data here, and check for the response status. If it's not 200, throw an error
+//       .then((response) => response.json())
+//       .then((data) =>
+//         document.write(
+//           `My first degree was a ${data.data[0].degrees.masters.data.type} from ${data.data[0].degrees.masters.data.school} in ${data.data[0].degrees.masters.data.program}`
+//         )
+//       )
+//         getDegrees("./degrees.json");
+//   }
+
+  clickBtn.addEventListener('click', getData)
+
 // }
   
 
